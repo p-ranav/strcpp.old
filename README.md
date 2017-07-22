fcddf45b8a2ccca8630221a67a98df81b0fca0cf
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-22 11:48:03 -0400

    Updates to file structure

--- README.md
+++ README.md
@@ -38,16 +38,19 @@ int main() {
 
   std::string input = "ERROR::Houston, we have a problem!";
 
-  if (strcpp::contains(input, "ERROR")) {               // containment check - case sensitive
-    std::cout << "We have a problem!" << std::endl;
+  if (strcpp::contains(input, "ERROR")) {                               // containment check - case sensitive
+    std::cout << "Input string contains \"ERROR\"" << std::endl;        // check returns true and prints message
   }
 
-  if (strcpp::contains(input, "error::", true)) {       // containment check - ignore case
-    std::cout << "We have a problem!" << std::endl;
+  if (strcpp::contains(input, "error::", true)) {                       // containment check - ignore case
+    std::cout << "Input string contains \"error::\"" << std::endl;      // check returns true and prints message
   }
 
-  if (strcpp::contains(input, "PROBLEM", true)) {       // containment check - ignore case
-    std::cout << "Message contains problem" << std::endl;
+  if (strcpp::contains(input, "PROBLEM")) {                             // containment check - ignore case
+    std::cout << "Input string contains \"PROBLEM\"\n";
+  }
+  else {
+    std::cout << "Input string does not contain \"PROBLEM\"\n";         // check returns false and prints message
   }
 
   return 0;
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,48 @@
-#include "strcpp_test.h"
+#include <strcpp.h>
+
+void split_test() {
+  std::string input = "We are the champions";     // input string
+  std::vector<std::string> result;                // result vector
+
+  result = strcpp::split(input, " ");             // result = ["We", "are", "the", "champions"]
+  result = strcpp::split(input, " a");            // result = ["We", "re the champions"]
+  result = strcpp::split(input, "ions");          // result = ["We are the champ"]
+
+  input = "a,b,c,d,e";
+  result = strcpp::split(input, ",");             // result = ["a", "b", "c", "d", "e"]
+
+  input = "a, b, c, d, e";
+  result = strcpp::split(input, ", ");            // result = ["a", "b", "c", "d", "e"]
+
+  input = "LOGGER::ERROR::Error Message!";
+  auto message = strcpp::split(input, "::")[2];   // message = "Error Message!"
+
+  auto length = strcpp::length(message);          // length = 14
+}
+
+void contains_test() {
+  std::string input = "ERROR::Houston, we have a problem!";
+
+  if (strcpp::contains(input, "ERROR")) {                               // containment check - case sensitive
+    std::cout << "Input string contains \"ERROR\"" << std::endl;        // check returns true and prints message
+  }
+
+  if (strcpp::contains(input, "error::", true)) {                       // containment check - ignore case
+    std::cout << "Input string contains \"error::\"" << std::endl;      // check returns true and prints message
+  }
+
+  if (strcpp::contains(input, "PROBLEM")) {                             // containment check - ignore case
+    std::cout << "Input string contains \"PROBLEM\"\n";
+  }
+  else {
+    std::cout << "Input string does not contain \"PROBLEM\"\n";         // check returns false and prints message
+  }
+
+}
 
 int main() {
   split_test();
   contains_test();
-
   system("PAUSE");
   return 0;
 }
